486645cfd21c82f7e7601b2ff2c79af473929664
JariKooij <jarikooij@gmail.com>
2025-10-01 01:17:40 +0200

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-16">
+      <h1 className="mb-4 text-4xl font-bold">Page Not Found</h1>
+      <p className="text-lg">Sorry, we could not find that page.</p>
+    </div>
+  );
+};
+
+export default NotFound;
