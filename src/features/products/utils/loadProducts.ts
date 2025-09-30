461e9fda05654673c7e87925b3029ed59eeac956
JariKooij <jarikooij@gmail.com>
2025-09-30 19:53:55 +0200

    update loadproducts with filter by id

--- src/features/products/utils/loadProducts.ts
+++ src/features/products/utils/loadProducts.ts
@@ -7,13 +7,19 @@ const productDataFilePath = "/src/features/products/data/products.json";
 
 /**
  * Load productdata from a JSON file.
+ * @param ids the ids of the products to load
  * @returns an array of products.
  */
-export const loadProducts = async (): Promise<Product[]> => {
+export const loadProducts = async (ids?: number[]): Promise<Product[]> => {
   const ProductDataJSONFile = await fs.readFile(
     process.cwd() + productDataFilePath,
     "utf8",
   );
 
-  return JSON.parse(ProductDataJSONFile);
+  const productData: Product[] = JSON.parse(ProductDataJSONFile);
+  const filteredProductData = ids?.length
+    ? productData.filter((product) => ids.includes(product.id))
+    : productData;
+
+  return filteredProductData;
 };
